59c26266ca9836caa033d7b8d968e8d2bb091701
JhonT01 <jftc_94@hotmail.com>
2021-12-09 00:53:20 +0000

    Falta poder eliminar y poner estilos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,20 +5,45 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = React.useState("");
+	const [list, setList] = React.useState([]);
+
+	//	let lista = [
+	//		<p>Comprar alimentos</p>,
+	//		<p>pasear al perro</p>,
+	//		<p>Hacer ejercicio</p>
+	//	];
+
+	const validateInput = () => {
+		if (inputValue === "") alert("The input cannot be empty");
+		else setList([...list, inputValue]);
+	};
+
+	const eliminarInput = index => {
+		newList = list;
+		newList.splice(index, 1);
+		setList(newList); // ['cero', 'cuatro']
+	};
+
+	console.log(inputValue);
+	console.log(list);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<input
+				type="text"
+				onChange={e => setInputValue(e.target.value)}
+				value={inputValue}
+			/>
+			<button onClick={validateInput}>Añadir</button>
+			{list.map((item, index) => (
+				<li key={index}>
+					<p>{item}</p>
+					<button onClick={eliminarInput(index)}>
+						X
+					</button>
+				</li>
+			))}
 		</div>
 	);
 };
